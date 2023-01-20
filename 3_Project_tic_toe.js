265898498824df92048b6ec08ce276f5fb6dc280
chithra98devi <44505676+chithra98devi@users.noreply.github.com>
2023-01-20 07:42:13 +0530

    Add files via upload

---
+++ 3_Project_tic_toe.js
@@ -0,0 +1,34 @@
+//Restart Game Button
+
+var restart = document.querySelector("#b");
+
+//Grabs all the squares
+
+var squares = document.querySelectorAll("td");
+//clear all the squares
+
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = "";
+  }
+}
+
+restart.addEventListener("click", clearBoard);
+
+//check the squares
+
+function changesMarker() {
+  if (this.textContent === "") {
+    this.textContent = "X";
+  } else if (this.textContent === "X") {
+    this.textContent = "O";
+  } else {
+    this.textContent = "";
+  }
+}
+
+//For loop to add event listeners to all squares
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener("click", changesMarker);
+}
